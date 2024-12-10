c26948fdddcc9f85a7b93dd4637977bedbca33ec
PascheK <killian.pasche7@gmail.com>
2024-12-10 12:42:12 +0000

    Fix deployement

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -83,22 +83,21 @@ export const verifySecret = async ({accountId, password} : {accountId: string, p
 export const getCurrentUser = async () => {
 
   try{
-    const {databases, account } = await createSessionClient();
+    const { databases, account } = await createSessionClient();
 
     const result = await account.get();
-  
+
     const user = await databases.listDocuments(
       appwriteConfig.databaseId,
       appwriteConfig.usersCollectionId,
-      [Query.equal("accountId", result.$id)]
+      [Query.equal("accountId", result.$id)],
     );
   
     if (user.total <= 0) return null;
   
     return parseStringify(user.documents[0]);
-  }catch(error){
-    handleError(error, "Failed to sign out user");
-
+  } catch (error) {
+    console.log(error);
   }
  
 }
